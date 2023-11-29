f818f0352b70d3fb3622f15eade36f75e005636e
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-11-28 18:57:48 -0800

    check

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    len(str)
+    for i in range(len):
+        if 96 < ord(s) < 123:
+            asi = chr(ord(asi) - 32)
+        print("{}".format(asi), end='')
+    print()

c122f981751b82c04e4cf413f9292298dad805ab
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-11-30 15:12:32 -0800

    check_2.4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     if strlen == 1:
         print("0 arguments.")
     else:
-        print("{:d} argument:".format(strlen - 1))
+        if strlen == 2:
+            print("{:d} argument:".format(strlen - 1))
+        else:
+            print("{:d} arguments:".format(strlen - 1))
         for i in range(strlen - 1):
             print("{:d}: {}".format(i + 1, sys.argv[i + 1]))

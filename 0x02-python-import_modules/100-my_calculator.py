38527f9087787e2348280b68a974d9b3e9ac4903
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-11-30 16:18:51 -0800

    check_100.0

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+
+    strlen = len(sys.argv)
+    if strlen < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if operator == "+":
+            print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+            exit(0)
+        elif operator == "-":
+            print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+            exit(0)
+        elif operator == "*":
+            print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+            exit(0)
+        elif operator == "/":
+            print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)

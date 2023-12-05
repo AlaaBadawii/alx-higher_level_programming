11d2cead282260f61e65234f875a7844980c00c6
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-04 17:54:34 -0800

    h

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,45 +1,50 @@
 #include "lists.h"
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: listint_t double_pinter
- * Return: Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-        listint_t *current, *temp;
-        int len, i, *str;
+    listint_t *original_head = *head;
+    listint_t *runner = *head;
+    int len = 0, i;
+    int *str;
 
-        current = *head;
-        temp = *head;
+    if (original_head == NULL)
+        return 1;
 
-        if (*head == NULL)
-                return 1;
-        while (temp != NULL)
-        {
-                temp = temp->next;
-                len++;
-        }
-        str = malloc(sizeof(int) * len / 2);
-        for (i = 0; i < len / 2; i++)
-        {
-                str[i] = current->n;
-                current = current->next;
-        }
-        if (len % 2 != 0)
-        {
-                current = current->next;
-                len--;
-        }
-        for (i = len / 2 - 1; i >= 0; i--)
-        {
-                if (current->n != str[i])
-                {
-                        free(str);
-                        return (0);
-                }
-                current  = current->next;
-        }
-        free(str);
+    while (runner != NULL)
+    {
+        runner = runner->next;
+        len++;
+    }
 
-        return (1);
+    str = malloc(sizeof(int) * len / 2);
+
+    for (i = 0; i < len / 2; i++)
+    {
+        str[i] = original_head->n;
+        original_head = original_head->next;
+    }
+
+    if (len % 2 != 0)
+    {
+        original_head = original_head->next;
+        len--;
+    }
+
+    for (i = len / 2 - 1; i >= 0; i--)
+    {
+        if (original_head->n != str[i])
+        {
+            free(str);
+            return 0;
+        }
+        original_head = original_head->next;
+    }
+
+    free(str);
+    return 1;
 }

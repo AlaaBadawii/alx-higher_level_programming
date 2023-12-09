f416f38cad14e081f66adfc46757e7b6e8acdcf7
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-08 18:58:40 -0800

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    result = 0
+    for i in roman_string:
+        if i == 'I':
+            result += 1
+        elif i == "V":
+            result += 5
+        elif i == "X":
+            result += 10
+        elif i == "L":
+            result += 50
+        elif i == "C":
+            result += 100
+        elif i == "D":
+            result += 500
+        elif i == "M":
+            result += 1000
+        else:
+            return 0
+    return result

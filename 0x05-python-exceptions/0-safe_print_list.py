0294ec8da53c98142802801887eca390cdacf800
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-18 18:07:03 -0800

    check_0.3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             real_num += 1
         except:
             return real_num
-        finally:
             print()
+    print()
     return real_num

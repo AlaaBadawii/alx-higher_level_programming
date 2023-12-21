1633051d6bbf803f65b272caa1c211dd7817f1e4
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-20 17:11:03 -0800

    check_3.1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """represent a Square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new square.
         Args:
             size (int): the size of the new square.

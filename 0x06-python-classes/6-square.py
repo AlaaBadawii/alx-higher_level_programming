e0903537a05b2bc1741004d35946b533a0949c28
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-20 18:24:36 -0800

    check_6.5

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,7 +43,7 @@ class Square:
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-    
+
     def area(self):
         """calc area"""
         return (self.__size * self.__size)

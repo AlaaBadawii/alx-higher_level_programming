614c90f1c06275786218199cf3f07f6729dab193
Alaa Ahmed <alaaahmed16112000@gmail.com>
2023-12-20 16:45:26 -0800

    check_1.0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+class Square:
+    """represent a Square"""
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): the size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

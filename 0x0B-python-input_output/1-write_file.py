f556fdd5d6d88604b2efdbc6257d6f7fbcdef738
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-09 13:10:24 -0800

    check_1.0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) 
+    Args:
+        filename (file): name of the file
+        text (string): string to be written
+    Returns:
+         returns the number of characters written
+    """
+    char_written = 0
+    with open(filename, 'w', encoding="utf-8") as f:
+        char_written = f.write(text)
+    return char_written

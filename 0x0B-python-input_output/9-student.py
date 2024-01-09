be0dfeb201c59fe2f86d6b691a0bcb9a250bb434
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-09 15:17:30 -0800

    check_9.0

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,24 +3,25 @@
 
 
 class Student:
-    """A class that represent a student"""
+    """A class the represent a student"""
 
     def __init__(self, first_name, last_name, age):
-        """sets attributes of a new student instance
+        """Sets attributes of a new Student instance
 
             Args:
-            first_name (str): first name
-            last_name (str): last name
-            age (int): the age
+                first_name (str): The first name
+                last_name (str): The last name
+                age (int): The age
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
-        """Return a json represent of student"""
+        """Returns a json represent of Student"""
         r = {}
         for key in self.__dict__:
             value = getattr(self, key)
-            if value in [bool, int, str, dict, list]:
+            if type(value) in [list, dict, str, int, bool]:
                 r[key] = value
         return r

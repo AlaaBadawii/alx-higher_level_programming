16d29176c2a7e698bf817ba7df2384db9e4254a4
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-09 13:01:16 -0800

    check_0.0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""``read_file`` module"""
+
+
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:
     Args:
         filename (file): name of the file
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        read_file = f.read()
-    print(read_file, end="")
+        for line in f:
+            print(line, end="")

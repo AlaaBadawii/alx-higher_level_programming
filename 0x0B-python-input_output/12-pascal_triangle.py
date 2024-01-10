eb350b6039a98903a911614fa3cecde4ea72ad86
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-09 16:40:49 -0800

    check_12.0

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,21 @@
-#!/usr/bin/python3
-"""pascal_triangle module"""
-
-
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+
+    Args:
+    - n (int): The number of rows to generate.
+
+    Returns:
+    - A list of lists representing Pascal's triangle.
+    """
     if n <= 0:
         return []
-    triangle = [[1]]
+
+    triangle = [[1]]  # The first row
+
     for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
+        # Calculate the next row based on the previous row
+        row = [1] + [triangle[i-1][j-1] + triangle[i-1][j] for j in range(1, i)] + [1]
         triangle.append(row)
+
     return triangle

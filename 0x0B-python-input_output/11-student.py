4844f3631cb53ddd2e3ceac1619ca5a816f1a16c
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-09 16:06:42 -0800

    check_11.0

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""``Student`` class module"""
+
+
+class Student:
+    """A class that represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """sets attributes of a new student instance
+
+            Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): the age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a json represent of student"""
+        r = {}
+        if attrs is not None:
+            keys = [key for key in self.__dict__ if key in attrs]
+        else:
+            keys = [key for key in self.__dict__]
+
+        for key in keys:
+            value = getattr(self, key)
+            if type(value) in [bool, int, str, dict, list]:
+                r[key] = value
+        return r
+    def reload_from_json(self, json):
+        """Replaces all attributs of Student instance from json"""
+        for key in json:
+            setattr(self, key, json[key])

0069738a309045f0023a29d7f6fee84a0ea585d9
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-10 13:51:25 -0800

    check_3.0

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class, otherwise False.
+    """
+    return issubclass(type(obj), a_class)

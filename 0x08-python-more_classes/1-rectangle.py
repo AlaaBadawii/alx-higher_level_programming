f3a303c970d8ad0547dbcfeb7b5170fb3c117afa
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-12 07:37:49 -0800

    check_1.0

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """definition class Rectangle with private attributes width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width=0, height=0):
+        def width(self):
+        def width(self, value):
+        def height(self):
+        def height(self, value):
+    """
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

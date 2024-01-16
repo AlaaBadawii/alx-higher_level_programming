f2309c9f48fac3e94654b21e5b8b96261366a4dc
Alaa Ahmed <alaaahmed16112000@gmail.com>
2024-01-15 16:07:52 -0800

    check

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,6 +77,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                                                        self.__y, self.__width,
                                                        self.__height)
+
     def update(self, *args, **kwargs):
         """
             assigns an argument to each attribute

fd7cf2500066282b64909a7973b0fa081694a24d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-02-15 20:11:07 +0200

    check

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT score, name IF name EXISTS
+SELECT score, name WHERE name IS NOT NULL
 ORDER BY SCORE DESC;

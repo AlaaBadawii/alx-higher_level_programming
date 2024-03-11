bb58ba71bb5f7baf6240bdf33575790b952cce35
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-03-11 23:41:06 +0200

    check

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  return a + b;
+  a = parseInt(a);
+  b = parseInt(b);
+
+  return (a + b);
 }
 
 const num1 = process.argv[2];
 const num2 = process.argv[3];
+const sum = add(num1, num2);
 
-if (num1 === undefined || num2 === undefined) {
-  console.log('NaN');
-} else {
-  console.log(add(num1, num2));
-}
+console.log(sum);

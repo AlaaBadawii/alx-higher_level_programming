a7f4a63332e4b6ea567581ab8c78ed4ee94c5e63
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-03-11 23:36:58 +0200

    check

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const num1 = process.argv[2];
 const num2 = process.argv[3];
 
 if (num1 === undefined || num2 === undefined) {
-  console.log(NaN);
+  console.log('NaN');
 } else {
   console.log(add(num1, num2));
 }

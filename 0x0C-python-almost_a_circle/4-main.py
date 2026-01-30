96ec36330054297fd1e098b9fc1508868d786ec3
Alaa Badawii <alaa.badawy404@gmail.com>
2026-01-30 16:50:24 +0200

    ADD update method for attr assignments

--- 0x0C-python-almost_a_circle/4-main.py
+++ 0x0C-python-almost_a_circle/4-main.py
@@ -4,19 +4,10 @@ from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 12, 0, 0)
-    r2 = Rectangle(10, 12, 1, 0)
-    r3 = Rectangle(5, 4, 4, 3)
-
+    r1 = Rectangle(4, 6)
     r1.display()
 
     print("---")
 
-    r2.display()
-
-    
-    print("---")
-
-    r3.display()
-
-
+    r1 = Rectangle(2, 2)
+    r1.display()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,23 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """Assigns an argument to each attribute,
+        1st: id
+        2nd: width
+        3rd: height
+        4th: x
+        5th: y
+        This type of argument is called a “no-keyword argument”
+        Argument order is super important."""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
